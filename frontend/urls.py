57b22df74acb7a2e9e4264d18fd6003dfbfb029d
Victory Ma <victoryma23@gmail.com>
2025-07-08 11:21:35 -0500

    Created logged out page, logout button works, and login styled

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
+from django.contrib import admin
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('tasks/', views.tasks, name="tasks"),
-    path('study_sessions', views.study_sessions, name="study_sessions"),
-    path('about/', views.about, name="about"),
-    path('profile/', views.profile, name="profile"),
-    path('contact/', views.contact, name="contact"),
+    path('', views.index, name='index'),
+    path('tasks/', views.tasks, name='tasks'),
+    path('study_sessions', views.study_sessions, name='study_sessions'),
+    path('about/', views.about, name='about'),
+    path('profile/', views.profile, name='profile'),
+    path('contact/', views.contact, name='contact'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
