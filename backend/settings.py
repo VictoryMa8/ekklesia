a7c09db4e53fe5f0ea516ccda8f7f4094fce26a1
Victory Ma <victoryma23@gmail.com>
2025-07-09 16:51:00 -0500

    Added a loading indicator to task creation with htmx

--- backend/settings.py
+++ backend/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+
+import psycopg2
 import os
 from dotenv import load_dotenv
 from pathlib import Path
@@ -79,8 +81,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.getenv('DB_HOST'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PORT': os.getenv('DB_PORT'),
+        'PASSWORD': os.getenv('DB_PASS'),
     }
 }
 
