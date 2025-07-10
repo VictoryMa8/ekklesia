f600a49160885211f8d29ac91e5e23b05d483e94
Victory Ma <victoryma23@gmail.com>
2025-07-10 18:26:19 -0500

    Custom user created named demos

--- frontend/models.py
+++ frontend/models.py
@@ -1,12 +1,18 @@
 from django.db import models
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 from uuid import uuid4
 
-User = get_user_model() 
+class Demos(AbstractUser):
+    tasks_completed = models.IntegerField(default=0)
+    cycles_completed = models.IntegerField(default=0)
+    total_time_studied = models.DurationField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username
 
 class Task(models.Model):
     uuid = models.UUIDField(default=uuid4, unique=True, primary_key=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(Demos, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     completed = models.BooleanField(default=False)
     title = models.CharField(max_length=100)
@@ -14,7 +20,7 @@ class Task(models.Model):
 
 class StudySession(models.Model):
     uuid = models.UUIDField(default=uuid4, unique=True, primary_key=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(Demos, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100)
     study_time = models.DurationField() # interval data type in Postgres
